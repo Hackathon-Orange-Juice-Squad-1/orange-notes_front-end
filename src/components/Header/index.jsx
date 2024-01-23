8ac28ade941957e444eaedff8075c2ea534dbd90
Guilherme Henrique de Castro <138685301+GuihCastro@users.noreply.github.com>
2024-01-23 16:13:54 -0300

    Add hamburger menu and toggle functionality to Header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,23 +1,35 @@
+import { useState } from "react";
 import logo from "../../assets/img/logo-orange-3x.png";
 import avatar from "../../assets/img/avatar-2x.png";
 import { FaBell } from "react-icons/fa6";
-import { Container, Nav, NavLinks, NavProfile } from "./style";
+import { Container, Nav, NavLinks, NavProfile, HamburgerButton } from "./styles";
 
 export const Header = () => {
+    const [isActive, setIsActive] = useState(false);
+    const ToggleIsActive = () => setIsActive(!isActive);
+
     return (
         <Container>
+            <HamburgerButton className={isActive ? "icon iconActive" : "icon"} onClick={ToggleIsActive}>
+                <div className="hamburger hamburgerIcon"></div>
+            </HamburgerButton>
+
             <a href="/">
                 <img src={logo} alt="Logo da aplicação Orange Portfolio" />
             </a>
 
-            <Nav>
-                <NavLinks>
+            <Nav> 
+                <NavLinks className={isActive ? "menuOpen" : "menuClosed"}>
                     <li>
                         <a href="/">Meus projetos</a>
                     </li>
 
                     <li>
                         <a href="/">Descobrir</a>
+                    </li>
+
+                    <li>
+                        <a href="/">Configurações</a>
                     </li>
                 </NavLinks>
 
---
+++ src/components/Header/styles.js
@@ -0,0 +1,184 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+    width: 100vw;
+    height: 7.3rem;
+
+    padding: 1.6rem 3.2rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 10rem;
+
+    background-color: ${({ theme }) => theme.COLORS.BLUE};
+
+    position: relative;
+
+    > a {
+        img {
+            height: 4rem;
+        }
+    }
+
+    @media (max-width: 425px) {
+        height: 6.4rem;
+        padding: 1.6rem 3rem 1.6rem 6.5rem;
+        flex-shrink: 0;
+
+        border-radius: 0 0 .5rem .5rem;
+
+        -webkit-box-shadow: 0px 0px 15px 0px rgba(0,0,0,0.6);
+        -moz-box-shadow: 0px 0px 15px 0px rgba(0,0,0,0.6);
+        box-shadow: 0px 0px 15px 0px rgba(0,0,0,0.6);
+    }
+`;
+
+export const HamburgerButton = styled.button`
+    position: absolute;
+    z-index: 1;
+    left: 3rem;
+
+    width: fit-content;
+    height: 4rem;
+
+    background-color: transparent;
+
+    border: none;
+
+    cursor: pointer;
+
+    > .hamburger {
+        position: absolute;
+        top: 50%;
+        left: 0;
+
+        width: 20px;
+        height: 2px;
+
+        background-color: ${({ theme }) => theme.COLORS.WHITE_2};
+        box-shadow: 0 2px 5px rgba(0, 0, 0, .2);
+        
+        transition: .5s ease-in-out;
+
+        &::before,
+        &::after {
+            content: '';
+
+            position: absolute;
+            left: 0;
+
+            width: 20px;
+            height: 2px;
+
+            background-color: ${({ theme }) => theme.COLORS.WHITE_2};
+            box-shadow: 0 2px 5px rgba(0, 0, 0, .2);
+        
+            transition: .5s ease-in-out;
+        }
+
+        &::before {
+            top: -6px;
+        }
+
+        &::after {
+            top: 6px;
+        }
+    }
+
+    @media (min-width: 425px) {
+        display: none;
+    }
+`;
+
+export const Nav = styled.nav`
+    width: 100%;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    @media (max-width: 425px) {
+        justify-content: flex-end;
+    }
+`;
+
+export const NavLinks = styled.ul`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 2.4rem;
+
+    > li {
+        a {
+            font-size: 2rem;
+            font-weight: 500;
+            letter-spacing: 0.15px;
+            color: ${({ theme }) => theme.COLORS.WHITE_2};
+        }
+
+        &:nth-last-child(1) {
+            display: none;
+        }
+    }
+
+    @media (max-width: 425px) {
+        &.menuClosed {
+            display: none;
+        }
+
+        &.menuOpen {
+            position: absolute;
+            top: 6.4rem;
+            left: 0;
+
+            width: 14.7rem;
+            height: 15.5rem;
+
+            flex-direction: column;
+            align-items: flex-start;
+            justify-content: flex-start;
+
+            padding: 1.6rem;
+            gap: 1.2rem;
+
+            background-color: ${({ theme }) => theme.COLORS.WHITE_1};
+
+            border-radius: 4px;
+
+            > li {
+                a {
+                    font-size: 1.6rem;
+                    font-weight: 100;
+                    color: ${({ theme }) => theme.COLORS.GRAY_8};
+                }
+
+                &:nth-last-child(1) {
+                    display: inline-block;
+                    padding-top: 1.2rem;
+                    border-top: 1px solid ${({ theme }) => theme.COLORS.GRAY_5};
+                }
+            }
+        }
+    }
+`;
+
+export const NavProfile = styled.ul`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1.6rem;
+
+    > li {
+        a {
+            font-size: 2rem;
+            font-weight: 500;
+            color: ${({ theme }) => theme.COLORS.WHITE_2};
+
+            img {
+                height: 4rem;
+                border-radius: 50%;
+            }
+        }
+    }
+`;
