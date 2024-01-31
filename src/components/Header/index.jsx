a523106d35de4b1de405f171619b93b374ce8028
Marcos Carvalho <gcmarcoss@gmail.com>
2024-01-30 21:34:38 -0300

    ✨ Discover page route No branch marcos Mudanças a serem submetidas: 	modified:   src/components/Header/index.jsx 	modified:   src/routes/app.routes.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,48 +5,48 @@ import { FaBell } from "react-icons/fa6";
 import { Container, Nav, NavLinks, NavProfile, HamburgerButton } from "./styles";
 
 export const Header = () => {
-    const [isActive, setIsActive] = useState(false);
-    const ToggleIsActive = () => setIsActive(!isActive);
+  const [isActive, setIsActive] = useState(false);
+  const ToggleIsActive = () => setIsActive(!isActive);
 
-    return (
-        <Container>
-            <HamburgerButton className={isActive ? "iconOpen" : "iconClosed"} onClick={ToggleIsActive}>
-                <div className="hamburger hamburgerIcon"></div>
-            </HamburgerButton>
+  return (
+    <Container>
+      <HamburgerButton className={isActive ? "iconOpen" : "iconClosed"} onClick={ToggleIsActive}>
+        <div className="hamburger hamburgerIcon"></div>
+      </HamburgerButton>
 
+      <a href="/">
+        <img src={logo} alt="Logo da aplicação Orange Portfolio" />
+      </a>
+
+      <Nav>
+        <NavLinks className={isActive ? "menuOpen" : "menuClosed"}>
+          <li>
+            <a href="/">Meus projetos</a>
+          </li>
+
+          <li>
+            <a href="/descobrir">Descobrir</a>
+          </li>
+
+          <li>
+            <a href="/">Configurações</a>
+          </li>
+        </NavLinks>
+
+        <NavProfile>
+          <li>
             <a href="/">
-                <img src={logo} alt="Logo da aplicação Orange Portfolio" />
+              <img src={avatar} alt="Avatar de usuário" />
             </a>
+          </li>
 
-            <Nav> 
-                <NavLinks className={isActive ? "menuOpen" : "menuClosed"}>
-                    <li>
-                        <a href="/">Meus projetos</a>
-                    </li>
-
-                    <li>
-                        <a href="/">Descobrir</a>
-                    </li>
-
-                    <li>
-                        <a href="/">Configurações</a>
-                    </li>
-                </NavLinks>
-
-                <NavProfile>
-                    <li>
-                        <a href="/">
-                            <img src={avatar} alt="Avatar de usuário" />
-                        </a>
-                    </li>
-
-                    <li>
-                        <a href="/">
-                            <FaBell />
-                        </a>
-                    </li>
-                </NavProfile>
-            </Nav>
-        </Container>
-    );
+          <li>
+            <a href="/">
+              <FaBell />
+            </a>
+          </li>
+        </NavProfile>
+      </Nav>
+    </Container>
+  );
 };
--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 
 import { Home } from "../pages/Home";
+import { Discover } from "../pages/Discover";
 
 export function AppRoutes() {
-    return (
-        <Routes>
-            <Route path="/" element={ <Home /> } />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/descobrir" element={<Discover />} />
+    </Routes>
+  );
 };
