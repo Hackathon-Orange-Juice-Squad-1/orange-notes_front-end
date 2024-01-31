7d692d9992154dbde14a21e2a378659ad024498b
Victor Hugo da Silva <silva.victor.hugo@outlook.com>
2024-01-30 23:13:26 -0300

    roolback da pagina App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,15 @@
-import { ThemeProvider } from "styled-components";
-import GlobalStyles from "./styles/globals";
-import theme from "./styles/theme";
-
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { Register } from "./pages/Register";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/Register",
-    element: <Register />,
-  },
-]);
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './styles/globals';
+import theme from './styles/theme';
+import { AppRoutes } from './routes';
 
 function App() {
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <Register />
-        <RouterProvider router={router} />
-      </ThemeProvider>
-    </>
-  );
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <AppRoutes />
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
