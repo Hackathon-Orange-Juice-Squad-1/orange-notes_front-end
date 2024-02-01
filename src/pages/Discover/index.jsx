55e0643b61e6bc417db04d22f4eb58e6d819de10
Marcos Carvalho <gcmarcoss@gmail.com>
2024-02-01 18:55:38 -0300

    ✨ Integrate modal No branch marcos Mudanças a serem submetidas: 	modified:   src/pages/Discover/index.jsx

--- src/pages/Discover/index.jsx
+++ src/pages/Discover/index.jsx
@@ -2,7 +2,6 @@ import { Container, Main, Projects, Thumbnail, Title } from "./styles";
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
 import { ProfileSmall } from "../../components/ProfileSmall";
-import projectBiancaG from "../../assets/img/projectBiancaG.png"
 import avatarBianca from "../../assets/img/avatarBianca.png"
 import { ModalPreview } from "../../components/ModalPreview";
 import { useState, useEffect } from "react";
@@ -10,7 +9,7 @@ import { useState, useEffect } from "react";
 
 export const Discover = () => {
 
-    const [isOpenModal, setOpenModal] = useState(false);
+    const [focusedProject, setFocusedProject] = useState(null);
 
     const [projectList, setProjectList] = useState([]);
 
@@ -35,7 +34,7 @@ export const Discover = () => {
 
                 <Projects>
                     {projectList.map((project) => (
-                        <Thumbnail key={project._id} onClick={() => setOpenModal(true)}>
+                        <Thumbnail key={project._id} onClick={() => setFocusedProject(project)}>
                             <img src={project.image.url} alt="Thumbnail de preview do projeto" />
 
                             <div>
@@ -46,14 +45,14 @@ export const Discover = () => {
                     <div>
                         <ModalPreview
                             avatar={avatarBianca}
-                            nome="Bianca Martin"
-                            data="02/24"
-                            title="Ecommerce One Page"
-                            image={projectBiancaG}
-                            description="Temos o prazer de compartilhar com vocês uma variação da nosso primeiro recurso gratuito, Monoceros. É um modelo de uma página para mostrar seus produtos. Tentamos redesenhar uma versão mais B2C e minimalista do nosso primeiro template de e-commerce."
-                            link="https://gumroad.com/products/wxCSL"
-                            onClick={() => setOpenModal(false)}
-                            preview={isOpenModal}>
+                            nome={focusedProject?.userName}
+                            data={focusedProject?.dataCriacao}
+                            title={focusedProject?.title}
+                            image={focusedProject?.image?.url}
+                            description={focusedProject?.desc}
+                            link={focusedProject?.link}
+                            onClick={() => setFocusedProject(null)}
+                            preview={focusedProject !== null}>
 
                         </ModalPreview>
                     </div>
