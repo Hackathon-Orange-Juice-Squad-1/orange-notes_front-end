300df362966124049ecea197c9726f1b33f687d8
Marcos Carvalho <gcmarcoss@gmail.com>
2024-02-03 23:38:40 -0300

    ♻️ Refactors the HTML to be semantic. No branch marcos Mudanças a serem submetidas: 	modified:   src/components/Input/index.jsx 	modified:   src/components/Modal/index.jsx 	modified:   src/components/Modal/styles.js 	modified:   src/hooks/useIsLoggedIn.jsx

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -4,7 +4,7 @@ export const Input = ({ label, name, value, ...rest }) => {
     return (
         <Container>
             <label htmlFor={name}>{label}</label>
-            <input id={name} placeholder={label} value={value} {...rest} />
+            <input id={name} name={name} placeholder={label} value={value} {...rest} />
         </Container>
     );
 };
