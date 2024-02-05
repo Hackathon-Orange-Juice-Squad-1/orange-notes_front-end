1c413cecca73adffebef2588a735f39c966adc5c
Marcos Carvalho <gcmarcoss@gmail.com>
2024-02-05 20:53:48 -0300

    💄 Refactor to use only styles No branch marcos Mudanças a serem submetidas: 	modified:   src/components/FormInput/index.jsx 	modified:   src/components/FormInput/styles.js

--- src/components/FormInput/index.jsx
+++ src/components/FormInput/index.jsx
@@ -1,24 +1,10 @@
-import { useState } from "react";
 import { Container } from "./styles";
 
-export const FormInput = ({ label, name, onClick, icon: Icon, ...rest }) => {
-    const [isActive, setIsActive] = useState(false);
-    const [value, setValue] = useState('');
-
-    function handleTextChange(text) {
-        setValue(text);
-    
-        if (text !== '') {
-            setIsActive(true);
-        } else {
-            setIsActive(false);
-        }
-    }
-
+export const FormInput = ({ label, value, name, onClick, onChange, icon: Icon, ...rest }) => {
     return (
-        <Container className={ isActive ? "active" : ""}>
+        <Container>
             { Icon && <button type="button" onClick={onClick}><img src={Icon} /></button> }
-            <input id={name} value={value} onChange={(e) => handleTextChange(e.target.value)} {...rest} />
+            <input id={name} value={value} placeholder={label} onChange={(e) => onChange(e.target.value)} {...rest} />
             <label htmlFor={name}>{label}</label>
         </Container>
     );
