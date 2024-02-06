d7e24c84a3d24c3c54f5683e412d1f5ffe5198ff
Marcos Carvalho <gcmarcoss@gmail.com>
2024-02-05 21:38:57 -0300

    🐛 Fix input error No branch marcos Mudanças a serem submetidas: 	modified:   src/components/FormInput/index.jsx

--- src/components/FormInput/index.jsx
+++ src/components/FormInput/index.jsx
@@ -4,7 +4,7 @@ export const FormInput = ({ label, value, name, onClick, onChange, icon: Icon, .
     return (
         <Container>
             { Icon && <button type="button" onClick={onClick}><img src={Icon} /></button> }
-            <input id={name} value={value} placeholder={label} onChange={(e) => onChange(e.target.value)} {...rest} />
+            <input id={name} value={value} placeholder={label} onChange={onChange ? (e) => onChange(e.target.value) : undefined} {...rest} />
             <label htmlFor={name}>{label}</label>
         </Container>
     );
