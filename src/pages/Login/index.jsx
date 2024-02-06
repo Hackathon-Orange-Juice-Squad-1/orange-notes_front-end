1a7dd1a74cd37c683a9743f76b9da2858ce80a42
Marcos Carvalho <gcmarcoss@gmail.com>
2024-02-05 21:08:44 -0300

    ⏪️ Redo google login using google scripts No branch marcos Mudanças a serem submetidas: 	new file:   src/components/GoogleLogin/index.jsx 	modified:   src/pages/Login/index.jsx 	modified:   src/pages/Login/styles.js

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,48 +1,29 @@
-import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
-import { jwtDecode } from "jwt-decode";
-
-import { handleLogin } from '../../services/loginAuthService';
+import { handleLogin } from "../../services/loginAuthService";
 
 import {
   Content,
   Container,
   ImageLogin,
-  ButtonGoogle,
-  LogoGoogle,
 } from "./styles";
 
 import { Form } from "../../components/Form";
 import { FormInput } from "../../components/FormInput";
 import { Button } from "../../components/Button";
+import { GoogleLogin } from "../../components/GoogleLogin";
 
 import imageLogin from "../../assets/img/img-login.png";
-import logoGoogle from "../../assets/img/logo-google.png";
 import imageIconClose from "../../assets/img/visibility-off.png";
 import imageIconOpen from "../../assets/img/visibility-on.png";
 
 import { Link } from "react-router-dom";
 
-const tokenClient = window.google.accounts.oauth2.initTokenClient({
-  client_id: "1095567261898-6eop9ig5u5fh70e5dqqan0empoiflbad.apps.googleusercontent.com",
-  scope: "https://www.googleapis.com/auth/userinfo.email",
-});
-
 export const Login = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const navigate = useNavigate();
 
   const handleClick = () => {
     setIsOpen(!isOpen);
-  };
-
-  const loginGoogle = () => {
-    tokenClient.callback = () => {
-      // TODO: Salve user at backend using this token
-      navigate('/register');
-    };
-    tokenClient.requestAccessToken({ prompt: "consent" });
   };
 
   const login = (event) => {
@@ -52,26 +33,17 @@ export const Login = () => {
 
     handleLogin(email.value, password.value)
       .then(() => location.reload())
-      .catch(error => alert(error.message));
+      .catch((error) => alert(error.message));
   };
 
   return (
-
     <Container>
       <ImageLogin src={imageLogin} alt="Imagem ilustrativa de Login" />
 
       <Content>
         <h1>Entre no Orange Portfólio</h1>
 
-        <ButtonGoogle type="button" onClick={loginGoogle}>
-          <LogoGoogle
-            src={logoGoogle}
-            alt="Logo Google"
-            width={18}
-            height={18}
-          />
-          Entrar com o Google
-        </ButtonGoogle>
+        <GoogleLogin />
 
         <Form title="Faça login com email" onSubmit={login}>
           <FormInput
@@ -92,10 +64,14 @@ export const Login = () => {
             onClick={handleClick}
           />
 
-          <Button type="submit" label="Entrar" style={{ width: "100%", marginBottom: "1.8rem" }} $primary />
+          <Button
+            type="submit"
+            label="Entrar"
+            style={{ width: "100%", marginBottom: "1.8rem" }}
+            $primary
+          />
 
           <Link to="/register">Cadastre-se</Link>
-
         </Form>
       </Content>
     </Container>
